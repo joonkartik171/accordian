57250f8cb5ba6e65ce29bfe879b04f9dc3bcfffd
kartik joon <kartikjoon171@gmail.com>
2024-02-21 14:28:32 +0530

    add

---
+++ script.js
@@ -0,0 +1,27 @@
+
+let c= document.getElementsByClassName("container")
+let b= document.getElementsByClassName("icon")
+let a= document.getElementsByClassName("ans")
+console.log(c)
+
+for(let i=0; i<c.length; i++){
+    c[i].addEventListener("click",function (){
+        a[i].style.transition="0.2s"
+       let res= c[i].classList.toggle("active")
+       
+       // b[i].style.rotate=" -180deg"
+        if(res === true){
+            b[i].style.rotate="135deg"
+            b[i].style.transition="0.2s"
+            b[i].style.color="dodgerblue"
+            
+        }
+        
+        else if(res ===false){
+            b[i].style.rotate="0deg"
+            b[i].style.transition="0.2s"
+            b[i].style.color="black"
+        }
+    })
+
+}
